e8c4238a06cdb09ffd8d54f552fe23866a9afb10
milos <milos.micovic12@gmail.com>
2023-09-06 16:44:04 +0200

    imported unique id

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ let input = document.getElementById("new-task-input");
 let message = document.getElementById("message");
 let posts = document.getElementById("tasks");
 
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   formValidation();
@@ -17,100 +19,78 @@ let formValidation = () => {
   }
 };
 
-let tasks = [];
-
-const saveTasksToLocalState = () =>
+const saveTasksToLocalState = () => {
   localStorage.setItem("tasks", JSON.stringify(tasks));
+};
 
 let renderTasks = () => {
   tasks.push({
+    id: Date.now(),
     text: input.value,
     isDone: false,
   });
   saveTasksToLocalState();
   displayHtml();
+  input.value = "";
 };
 
 let displayHtml = () => {
   posts.innerHTML = "";
-  tasks.map(
-    (task, index) =>
-      (posts.innerHTML += `
-      <div class="task"key="${index}" >
-      <input type="checkbox" id="checkbox${index}" ${
-        task.isDone ? "checked" : ""
-      }
-      
-       onChange="handleCheckbox(id,this)" 
-      />
-      <div class="content" style="text-decoration: ${
-        task.isDone ? "line-through" : "none"
-      };">
-       ${task.text}
-      </div>
-      <div class="actions">
-        <button  onClick="editPost(this)" class="edit">Edit</button>
-        <button onClick="deletePost(this)" class="delete">Delete</button>
-      </div>
-      </div>
-      
-      
-     `)
-  );
-
-  input.value = "";
+  tasks.map((task) => {
+    posts.innerHTML += `
+            <div class="task" data-id="${task.id}">
+                <input type="checkbox" ${task.isDone ? "checked" : ""} 
+                    onchange="handleCheckbox(this)" />
+                <div class="content" style="text-decoration: ${
+                  task.isDone ? "line-through" : "none"
+                };">
+                    <span class="task-text">${task.text}</span>
+                </div>
+                <div class="actions">
+                    <button onclick="editPost(this)">Edit</button>
+                    <button onclick="deletePost(this)">Delete</button>
+                </div>
+            </div>
+        `;
+  });
 };
 
-let deletePost = (e) => {
-  e.parentElement.parentElement.remove();
-  tasks.splice(e.parentElement.parentElement.id, 1);
+let deletePost = (button) => {
+  const taskId = button.parentElement.parentElement.getAttribute("data-id");
+  tasks = tasks.filter((task) => task.id !== Number(taskId));
   saveTasksToLocalState();
+  displayHtml();
 };
-// ...
 
-let editPost = (e) => {
-  const selectedTask = e.parentElement.parentElement;
-  const taskIndex = selectedTask.getAttribute("key");
-
-  const contentDiv = selectedTask.querySelector(".content");
-  const editButton = selectedTask.querySelector(".edit");
-
-  if (tasks[taskIndex]) {
-    if (tasks[taskIndex].isEditing) {
-      const updatedText = contentDiv.querySelector("input").value;
-      tasks[taskIndex].text = updatedText;
-      contentDiv.innerHTML = `<span style="font-size: inherit">${updatedText}</span>`;
-      editButton.textContent = "Edit";
+let editPost = (button) => {
+  const taskId = button.parentElement.parentElement.getAttribute("data-id");
+  const task = tasks.find((t) => t.id === Number(taskId));
+  if (task) {
+    const contentDiv =
+      button.parentElement.parentElement.querySelector(".content");
+    if (!task.isEditing) {
+      contentDiv.innerHTML = `<input type="text" value="${task.text}" style="font-size: inherit" />`;
+      button.textContent = "Update";
     } else {
-      const taskText = tasks[taskIndex].text;
-      contentDiv.innerHTML = `<input type="text" value="${taskText}" style="font-size: inherit" />`;
-      editButton.textContent = "Update";
-      tasks[taskIndex].currentText = taskText;
+      const updatedText = contentDiv.querySelector("input").value;
+      task.text = updatedText;
+      contentDiv.innerHTML = `<span style="font-size: inherit">${updatedText}</span>`;
+      button.textContent = "Edit";
     }
-
-    tasks[taskIndex].isEditing = !tasks[taskIndex].isEditing;
+    task.isEditing = !task.isEditing;
     saveTasksToLocalState();
   }
 };
 
-function handleCheckbox(id, el) {
-  let checkbox = document.getElementById(id);
-
-  const taskText = el.parentElement.children[1];
-  const parentKey = el.parentElement.getAttribute("key");
-
-  tasks[parentKey] ? (tasks[parentKey].isDone = checkbox.checked) : null;
-
-  saveTasksToLocalState();
-
-  if (checkbox.checked) {
-    taskText.style.textDecoration = "line-through";
-  } else {
-    taskText.style.textDecoration = "none";
+function handleCheckbox(checkbox) {
+  const taskId = checkbox.parentElement.getAttribute("data-id");
+  const task = tasks.find((t) => t.id === Number(taskId));
+  if (task) {
+    task.isDone = checkbox.checked;
+    const taskText = checkbox.parentElement.querySelector(".content");
+    taskText.style.textDecoration = task.isDone ? "line-through" : "none";
+    saveTasksToLocalState();
   }
 }
 
-(() => {
-  tasks = JSON.parse(localStorage.getItem("tasks")) || [];
-  displayHtml();
-})();
+displayHtml();
